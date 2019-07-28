515fdfa8b46a73d4e18e39ac515546ff940e954e
Peter Müller <peter@crycode.de>
2019-07-28 12:52:15 +0200

    Updated build files

--- build/lib/tools.js
+++ build/lib/tools.js
@@ -1,5 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Parse a hex or decimal number string.
+ * @param num The number as a string (e.g. 0x42 or 127)
+ * @return    The parsed number
+ */
 function parseNumber(num) {
     if (num.startsWith('0x')) {
         return parseInt(num, 16);
@@ -9,6 +14,11 @@ function parseNumber(num) {
     }
 }
 exports.parseNumber = parseNumber;
+/**
+ * Parse an address to a string.
+ * @param addr The address as a hex or decimal number string or `*`.
+ * @return     The address as a number or null if `addr` was `*`.
+ */
 function parseAddress(addr) {
     if (typeof addr === 'number')
         return addr;
@@ -17,6 +27,11 @@ function parseAddress(addr) {
     return parseNumber(addr);
 }
 exports.parseAddress = parseAddress;
+/**
+ * Format a number as a hex number string.
+ * @param num The number.
+ * @retrun    The hex number with leading `0x`.
+ */
 function hexNumber(num) {
     let s = num.toString(16).toUpperCase();
     if (s.length < 2) {
@@ -25,6 +40,12 @@ function hexNumber(num) {
     return '0x' + s;
 }
 exports.hexNumber = hexNumber;
+/**
+ * Round a floating point number to the given precision.
+ * @param num       The number.
+ * @param precision The number of decimals to round to.
+ * @return          The rounded number.
+ */
 function round(num, precision) {
     if (precision == 0)
         return Math.round(num);
@@ -35,6 +56,11 @@ function round(num, precision) {
     return Math.round(num * exp) / exp;
 }
 exports.round = round;
+/**
+ * Format a buffer as a hex string.
+ * @param buf The buffer to format.
+ * @return    A string with the buffer content as hex numbers separated by spaces.
+ */
 function formatBufferAsHexString(buf) {
     return buf.toString('hex').toUpperCase().replace(/(..)/g, ' 0x$1').trim();
 }
